a2011503a021353b9a411955cad77d0da29e2087
Juan Benjumea <benjumeamoreno@gmail.com>
2025-06-08 16:39:09 +0200

    v1.1

--- api/app.py
+++ api/app.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import joblib
 import pandas as pd
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 # Load model
-model = joblib.load("../models/best_model.pkl")
+model = joblib.load("../models/Extra_Trees_v1.pkl")
 
-## Imput schema
+## Input schema
 class MushroomInput(BaseModel):
     stem_length: float
     stem_width: float
